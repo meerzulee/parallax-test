4ccaa77a3016c2ae88c4f8cd70319797c723706d
meerzulee <meerzulee@gmail.com>
2020-05-01 19:48:51 +0600

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <div id="app">
+    <!-- <Parallax></Parallax> -->
+
+    <h1 class="text-center">Simple parallax</h1>
+    <div class="parallax-effect-container">
+      <div class="parallax-effect-image first"></div>
+      <div class="parallax-effect-content">
+        <h2 class="text-center invert">I'd like more simple CSS effects please</h2>
+      </div>
+    </div>
+    <div class="parallax-effect-container">
+      <div class="parallax-effect-image second"></div>
+      <div class="parallax-effect-content">
+        <h2 class="text-center invert">I'd like more simple CSS effects please</h2>
+      </div>
+    </div>
+    <div class="parallax-effect-container">
+      <div class="parallax-effect-image third"></div>
+      <div class="parallax-effect-content-end">
+        <h2 class="text-center invert">I'd like more simple CSS effects please</h2>
+      </div>
+    </div>
+    <h1 class="text-center">Simple parallax</h1>
+  </div>
+</template>
+
+<script>
+// import Parallax from "./components/Parallax.vue";
+
+export default {
+  name: "App",
+  components: {
+    // Parallax
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Cabin");
+
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Cabin", sans-serif;
+  padding: 200px 0px;
+  color: #545454;
+}
+a {
+  color: inherit;
+}
+p {
+  text-align: right;
+  margin-bottom: 16px;
+}
+.first {
+  background-image: url("./assets/1.png");
+}
+.second {
+  background-image: url("./assets/2.png");
+}
+.third {
+  background-image: url("./assets/3.png");
+}
+
+.parallax-effect-container {
+  height: 100vh;
+  overflow: hidden;
+  display: block;
+  background: black;
+
+  position: relative;
+  overflow: hidden;
+}
+.parallax-effect-content {
+  position: absolute;
+  display: block;
+  max-width: 100%;
+  top: 50%;
+  left: 0;
+  padding: 16px;
+  width: 100%;
+  transform-style: flat;
+  transform: translate3d(0, -50%, 0);
+}
+.parallax-effect-content-end {
+  
+  display: block;
+  max-width: 100%;
+  height: 100vh;
+
+  padding: 16px;
+  width: 100%;
+  transform-style: flat;
+  transform: translate3d(0, -50%, 0);
+}
+
+.parallax-effect-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  bottom: 0;
+  right: 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: scroll;
+  background-attachment: fixed;
+}
+
+.text-center {
+  text-align: center;
+  font-size: 52px;
+  margin-bottom: 22px;
+  display: block;
+  box-sizing: border-box;
+  max-width: 100%;
+}
+
+.invert {
+  color: white;
+}
+</style>
---
+++ src/components/Parallax.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div style="background-color: #fff; height: 100vh;">
+      <h1 style="margin-top: 0; padding-top: 20px;">Scroll down ⬇</h1>
+    </div>
+    <div style="position: relative; z-index: 9999; background-color: #fff;">
+      <h1 style="margin:0;">Parallax Effect</h1>
+      <parallax>
+        <img src="../assets/1.png" />
+      </parallax>
+    </div>
+
+    <h1>Parallax fixed position</h1>
+
+    <div style="position: relative;">
+      <parallax :fixed="true">
+        <img src="../assets/2.png" />
+      </parallax>
+    </div>
+
+    <div style="background-color: #fff; height: 100vh;"></div>
+  </div>
+</template>
+
+<script>
+import Parallax from "vue-parallaxy";
+export default {
+  components: {
+    Parallax
+  }
+};
+</script>
